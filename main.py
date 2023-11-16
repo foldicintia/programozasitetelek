d4e3a3afbaf95c6e97282a1f5b6dd607c186faec
foldicintia <foldicintia2002@gmail.com>
2023-11-16 12:05:23 +0100

    tételek

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import feladatok
-
+import szovegkezeles
+"""
 feladatok.feladat1(20, 52)
 
-"""
+
 írj függvényt, amely generál 10db véletlen számot: -10 és 10 között.
 Számold meg hány negatív szám van közötte.
 A visszatérési érték negatív számok száma.
-"""
+
 
 negativ_db: int = feladatok.negativ()
 print(f"A negatív számok száma: {negativ_db}")
 
 
-"""
+
 Írj függvényt, amely generál 8 db véletlen számot, [20,50) intervallumban, és visszatér ezek közül a legnagyobb.
-"""
+
 
 vel_osszeg: int = feladatok.veletlen_szam()
 print(f"A véletlen számok összege: {vel_osszeg}")
 
 
-"""
+
 Kérjünk be 3 db egész számot.
 Mekkora a számok átlaga?
-"""
+
 feladatok.feladat3()
 feladatok.atlag()
 
 feladatok.szamok()
+""" 
+db: int = szovegkezeles.szokoz("Indul a kutya és a tyúk aludni - árvíztűrő tükörfúrógép")
+print(f"A szóközök száma: {db}")
+
+uj_szoveg: str = szovegkezeles.szokoz_nelkul("Indul a kutya és a tyúk aludni - árvíztűrő tükörfúrógép")
+print(f"A megadott szöveg szóközök nélkül: {uj_szoveg}")
 
